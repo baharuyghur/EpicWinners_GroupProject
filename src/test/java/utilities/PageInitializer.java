e8094d9171f05b5775123a8f6b1972307ddcc495
PKLaptop <kanlayanacholp@gmail.com>
2023-04-05 19:34:00 -0400

    added PKTestCases

--- src/test/java/utilities/PageInitializer.java
+++ src/test/java/utilities/PageInitializer.java
@@ -1,19 +1,24 @@
 package utilities;
 
+import pages.PKAddressPage;
+import pages.LoginPage;
 
 import pages.Bahar_Pages.LoginPage_Bahar;
 import pages.Bahar_Pages.SigninPage_Bahar;
 
 public class PageInitializer extends BaseClass{
 	
+	public static LoginPage lp;
+	public static PKAddressPage addressPage;
 	public static LoginPage_Bahar lpb;
 	public static SigninPage_Bahar spb;
-	
 
-public static void initialize() {
-	lpb = new LoginPage_Bahar();
-	spb=new SigninPage_Bahar();
-	
-}
 
+	public static void initialize() {
+		lp = new LoginPage();
+		addressPage = new PKAddressPage();
+
+		lpb = new LoginPage_Bahar();
+		spb=new SigninPage_Bahar();
+	}
 }
