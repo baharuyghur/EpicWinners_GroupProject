5f303c53fee8169a63bf56b49cf2ac32b1de8ee7
Bahar <119145963+baharuyghur@users.noreply.github.com>
2023-04-08 17:52:44 -0400

    Update PageInitializer.java

--- src/test/java/utilities/PageInitializer.java
+++ src/test/java/utilities/PageInitializer.java
@@ -1,20 +1,7 @@
 package utilities;
 
-<<<<<<< HEAD
-import pages.ClinicPage;
-import pages.LoginPage;
-
-public class PageInitializer extends BaseClass {
-
-	public static LoginPage lp;
-	public static ClinicPage cp;
-
-public static void initialize() {
-	lp = new LoginPage();
-	cp = new ClinicPage();
-}
-=======
 import pages.PKAddressPage;
+import pages.ClinicPage;
 import pages.LoginPage;
 
 import pages.Bahar_Pages.LoginPage_Bahar;
@@ -28,6 +15,7 @@ public class PageInitializer extends BaseClass{
 	public static LoginPage_Bahar lpb;
 	public static SigninPage_Bahar spb;
 	public static HomePage hp;
+	public static ClinicPage cp;
 
 
 	public static void initialize() {
@@ -36,6 +24,6 @@ public class PageInitializer extends BaseClass{
 		lpb = new LoginPage_Bahar();
 		spb=new SigninPage_Bahar();
     hp= new HomePage();
+    cp= new ClinicPage();
 	}
->>>>>>> 1017f10f48e9a28615b6decb146dd34b21fc6fee
 }
